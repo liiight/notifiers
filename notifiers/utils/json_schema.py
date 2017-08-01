7e669054c872eafe307a7b79064f99f092421dbb
orcarmi <or.carmi82@gmail.com>
2017-08-01 11:33:51 +0300

    Started docs

--- notifiers/utils/json_schema.py
+++ notifiers/utils/json_schema.py
@@ -1,7 +1,10 @@
 def one_or_more(schema: dict, unique_items: bool = True) -> dict:
     """
     Helper function to construct a schema that validates items matching `schema` or an array
-    containing items matching `schema`.
+     containing items matching `schema`.
+
+    :param schema: The schema to use
+    :param unique_items: Flag if array items should be unique
     """
     return {
         'oneOf': [
