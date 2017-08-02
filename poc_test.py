a7e05a46011ddd50bcc67ccef1ea05d6cfa355ba
Or Carmi <or.carmi82@gmail.com>
2017-08-02 18:08:52 +0300

    Delete poc_test.py

--- poc_test.py
+++
@@ -1,4 +0,0 @@
-from notifiers.providers.pushover import Pushover
-
-data = {'title': 'bla', 'message': 'foo', 'user': 'uFMARdqgXPGXM6jx549jpT7Nk4Prn3', 'token': 'aPwSHwkLcNaavShxktBpgJH4bRWc3m'}
-Pushover().notify(data)
