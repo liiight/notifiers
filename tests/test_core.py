94c7ca02f191d76fc1a1df45b919282e6f3d3364
orcarmi <or.carmi82@gmail.com>
2017-08-03 21:41:59 +0300

    More tests

--- tests/test_core.py
+++ tests/test_core.py
@@ -34,15 +34,29 @@ def mock_provider() -> NotificationProvider:
 
 class TestCore(object):
     """Test core classes"""
+    valid_data = {'required': 'foo',
+                  'not_required': ['foo', 'bar']}
 
     def test_sanity(self, mock_provider):
         p = mock_provider()
         assert p.metadata == {'base_url': 'https://api.mock.com',
                               'provider_name': 'mock_provider',
                               'site_url': 'https://www.mock.com'}
-        assert p.arguments == {'not_required': {'type': 'string'},
-                               'required': {'type': 'string'}}
+        assert p.arguments == {
+            'not_required': {
+                'oneOf': [
+                    {'items': {'type': 'string'}, 'minItems': 1, 'type': 'array', 'uniqueItems': True},
+                    {'type': 'string'}
+                ]
+            },
+            'required': {'type': 'string'}
+        }
+
         assert p.required == ['required']
+        rsp = p.notify(**self.valid_data)
+        assert isinstance(rsp, NotificationResponse)
+        assert not rsp.errors
+        assert rsp.raise_on_errors() is None
 
     @pytest.mark.parametrize('data', [
         pytest.param({'not_required': 'foo'}, id='Missing required'),
@@ -61,3 +75,9 @@ class TestCore(object):
         data = {'foo': 'bar'}
         with pytest.raises(SchemaError):
             p.notify(**data)
+
+    def test_prepare_data(self, mock_provider):
+        p = mock_provider()
+        rsp = p.notify(**self.valid_data)
+        assert rsp.data == {'not_required': 'foo,bar',
+                            'required': 'foo'}
