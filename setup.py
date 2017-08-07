4da70e385662911a0d890df559482a1ae4cb8fbe
orcarmi <or.carmi82@gmail.com>
2017-08-07 17:21:30 +0300

    Forgot init file

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 
 setup(
     name='notifiers',
-    version='0.2',
+    version='0.3',
     packages=['notifiers', 'notifiers.providers'],
     url='https://github.com/liiight/notifiers',
     license='MIT',
     author='Or Carmi',
-    download_url='https://github.com/liiight/notifiers/archive/0.1.tar.gz',
+    download_url='https://github.com/liiight/notifiers/archive/0.3.tar.gz',
     author_email='or.carmi82@gmail.com',
     description='Easily send notifications everywhere'
 )
