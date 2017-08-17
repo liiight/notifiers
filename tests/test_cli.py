b151ba3b22ff932684397e74c3799dbc244f16c7
orcarmi <or.carmi82@gmail.com>
2017-08-17 09:09:23 +0300

    Created stubs for cli tests

---
+++ tests/test_cli.py
@@ -0,0 +1,23 @@
+from click.testing import CliRunner
+
+
+class TestCLI(object):
+    """CLI tests"""
+
+    def test_bad_notify(self):
+        """Test invalid notification usage"""
+
+    def test_notify_sanity(self):
+        """Test valid notification usage"""
+
+    def test_providers(self):
+        """Test providers command"""
+
+    def test_metadata(self):
+        """Test metadata command"""
+
+    def test_required(self):
+        """Test metadata command"""
+
+    def test_arguments(self):
+        """Test metadata command"""
