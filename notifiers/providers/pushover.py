25e062b11465ba3f9fe62f0d7498685c84312d10
orcarmi <or.carmi82@gmail.com>
2017-09-07 23:17:51 +0300

    improved pushover schema appearance

--- notifiers/providers/pushover.py
+++ notifiers/providers/pushover.py
@@ -18,50 +18,77 @@ class Pushover(Provider):
         return {
             'type': 'object',
             'properties': {
-                'user': one_or_more({'type': 'string',
-                                     'title': 'the user/group key (not e-mail address) of your user (or you)'}),
-                'message': {'type': 'string',
-                            'title': 'your message'},
-                'title': {'type': 'string',
-                          'title': 'your message\'s title, otherwise your app\'s name is used'},
-                'token': {'type': 'string',
-                          'title': 'your application\'s API token'},
-                'device': one_or_more(
-                    {'type': 'string',
-                     'title': 'your user\'s device name to send the message directly to that device'}),
-                'priority': {'oneOf': [
-                    {'type': 'number', 'minimum': -2, 'maximum': 2},
-                    {'type': 'string'}],
-                    'title': 'notification priority'},
-                'url': {'type': 'string',
-                        'format': 'uri',
-                        'title': 'a supplementary URL to show with your message'},
-                'url_title': {'type': 'string',
-                              'title': 'a title for your supplementary URL, otherwise just the URL is shown'},
-                'sound': {'type': 'string',
-                          'title': 'the name of one of the sounds supported by device clients to override the '
-                                   'user\'s default sound choice',
-                          'enum': self.sounds},
-                'timestamp': {'type': 'integer',
-                              'minimum': 0,
-                              'title': 'a Unix timestamp of your message\'s date and time to display to the user, '
-                                       'rather than the time your message is received by our API'},
-                'retry': {'type': 'integer',
-                          'minimum': 30,
-                          'title': 'how often (in seconds) the Pushover servers will send the same notification to the '
-                                   'user. priority must be set to 2'},
-                'expire': {'type': 'integer',
-                           'maximum': 86400,
-                           'title': 'how many seconds your notification will continue to be retried for. '
-                                    'priority must be set to 2'},
-                'callback': {'type': 'string',
-                             'format': 'uri',
-                             'title': 'a publicly-accessible URL that our servers will send a request to when the user'
-                                      ' has acknowledged your notification. priority must be set to 2'},
-                'html': {'type': 'integer',
-                         'minimum': 0,
-                         'maximum': 1,
-                         'title': 'enable HTML formatting'}
+                'user': one_or_more({
+                    'type': 'string',
+                    'title': 'the user/group key (not e-mail address) of your user (or you)'
+                }),
+                'message': {
+                    'type': 'string',
+                    'title': 'your message'
+                },
+                'title': {
+                    'type': 'string',
+                    'title': 'your message\'s title, otherwise your app\'s name is used'
+                },
+                'token': {
+                    'type': 'string',
+                    'title': 'your application\'s API token'
+                },
+                'device': one_or_more({
+                    'type': 'string',
+                    'title': 'your user\'s device name to send the message directly to that device'
+                }),
+                'priority': {
+                    'type': 'number',
+                    'minimum': -2,
+                    'maximum': 2,
+                    'title': 'notification priority'
+                },
+                'url': {
+                    'type': 'string',
+                    'format': 'uri',
+                    'title': 'a supplementary URL to show with your message'
+                },
+                'url_title': {
+                    'type': 'string',
+                    'title': 'a title for your supplementary URL, otherwise just the URL is shown'
+                },
+                'sound': {
+                    'type': 'string',
+                    'title': 'the name of one of the sounds supported by device clients to override the '
+                             'user\'s default sound choice',
+                    'enum': self.sounds
+                },
+                'timestamp': {
+                    'type': 'integer',
+                    'minimum': 0,
+                    'title': 'a Unix timestamp of your message\'s date and time to display to the user, '
+                             'rather than the time your message is received by our API'
+                },
+                'retry': {
+                    'type': 'integer',
+                    'minimum': 30,
+                    'title': 'how often (in seconds) the Pushover servers will send the same notification to the '
+                             'user. priority must be set to 2'
+                },
+                'expire': {
+                    'type': 'integer',
+                    'maximum': 86400,
+                    'title': 'how many seconds your notification will continue to be retried for. '
+                             'priority must be set to 2'
+                },
+                'callback': {
+                    'type': 'string',
+                    'format': 'uri',
+                    'title': 'a publicly-accessible URL that our servers will send a request to when the user'
+                             ' has acknowledged your notification. priority must be set to 2'
+                },
+                'html': {
+                    'type': 'integer',
+                    'minimum': 0,
+                    'maximum': 1,
+                    'title': 'enable HTML formatting'
+                }
             },
             'required': ['user', 'message', 'token'],
             'additionalProperties': False
