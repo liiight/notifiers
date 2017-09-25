0b4a0e9801b5d628a6b9a321aa54d3c132f8943b
orcarmi <or.carmi82@gmail.com>
2017-09-25 22:44:59 +0300

    Added support for missing chats

--- notifiers_cli/providers.py
+++ notifiers_cli/providers.py
@@ -13,11 +13,15 @@ def telegram():
 
 @telegram.command()
 @click.argument('token')
-def updates(token):
+@click.pass_context
+def updates(ctx, token):
     """Get a list of active chat IDs for your bot. If the list is empty, or you don't see a specific chat in here,
      send any message to your bot in the request chat"""
     from notifiers.providers.telegram import Telegram
     updates = Telegram().updates(token)
+    if not updates:
+        click.echo(f'Bot has not active chats! Send it ANY message and try again')
+        ctx.exit()
     chat_ids = defaultdict(dict)
     for update in updates:
         chat_ids[update['message']['chat']['id']] = update['message']['chat']
