df8b80e7ee2eb0696b1e75d52af8ed3fae6097b1
orcarmi <or.carmi82@gmail.com>
2017-09-26 00:23:42 +0300

    added more tests

--- tests/providers/test_telegram.py
+++ tests/providers/test_telegram.py
@@ -1,7 +1,7 @@
 import pytest
 
 from notifiers import get_notifier
-from notifiers.exceptions import BadArguments
+from notifiers.exceptions import BadArguments, NotificationError
 
 
 class TestTelegram:
@@ -28,3 +28,45 @@ class TestTelegram:
         with pytest.raises(BadArguments) as e:
             p.notify(**data)
         assert f'\'{message}\' is a required property' in e.value.message
+
+    def test_bad_token(self):
+        p = get_notifier('telegram')
+        data = {
+            'token': 'foo',
+            'chat_id': 1,
+            'message': 'foo'
+        }
+        with pytest.raises(NotificationError) as e:
+            rsp = p.notify(**data)
+            rsp.raise_on_errors()
+        assert 'Not Found' in e.value.message
+
+    @pytest.mark.online
+    def test_missing_chat_id(self):
+        p = get_notifier('telegram')
+        data = {
+            'chat_id': 1,
+            'message': 'foo'
+        }
+        with pytest.raises(NotificationError) as e:
+            rsp = p.notify(**data)
+            rsp.raise_on_errors()
+        assert 'chat not found' in e.value.message
+
+    @pytest.mark.online
+    def test_sanity(self):
+        p = get_notifier('telegram')
+        rsp = p.notify(message='foo')
+        rsp.raise_on_errors()
+
+    @pytest.mark.online
+    def test_all_options(self):
+        p = get_notifier('telegram')
+        data = {
+            'parse_mode': 'markdown',
+            'disable_web_page_preview': True,
+            'disable_notification': True,
+            'message': '_foo_'
+        }
+        rsp = p.notify(**data)
+        rsp.raise_on_errors()
