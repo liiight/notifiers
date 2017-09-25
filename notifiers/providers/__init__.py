e9804b0b9905dce99e4db01c89257a36374b47e3
orcarmi <or.carmi82@gmail.com>
2017-09-25 18:20:54 +0300

    created telegram provider

--- notifiers/providers/__init__.py
+++ notifiers/providers/__init__.py
@@ -1,9 +1,10 @@
-from . import pushover, simplepush, slack, email, gmail
+from . import pushover, simplepush, slack, email, gmail, telegram
 
 _all_providers = {
     'pushover': pushover.Pushover,
     'simplepush': simplepush.SimplePush,
     'slack': slack.Slack,
     'email': email.SMTP,
-    'gmail': gmail.Gmail
+    'gmail': gmail.Gmail,
+    'telegram': telegram.Telegram
 }
