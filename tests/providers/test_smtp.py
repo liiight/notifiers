51f387fcc4b8eb6be40c9b6d2ee9977f3e312d96
orcarmi <or.carmi82@gmail.com>
2017-11-06 10:17:31 +0200

    more informative error

--- tests/providers/test_smtp.py
+++ tests/providers/test_smtp.py
@@ -36,10 +36,11 @@ class TestSMTP(object):
         with pytest.raises(NotificationError) as e:
             rsp = p.notify(**data)
             rsp.raise_on_errors()
-        possible_errors =  ['Errno 111', 'Errno 61', 'Errno 8']
-        print(e.value.message)
-        assert any(error in e.value.message for error in possible_errors)
-        assert any(error in rsp_error for rsp_error in rsp.errors for error in possible_errors)
+        possible_errors = ['Errno 111', 'Errno 61', 'Errno 8']
+        assert any(error in e.value.message for error in possible_errors), \
+            f'Error not in expected errors; {e.value.message}'
+        assert any(error in rsp_error for rsp_error in rsp.errors for error in possible_errors), \
+            f'Error not in expected errors; {rsp.errors}'
 
     @pytest.mark.online
     def test_smtp_sanity(self):
