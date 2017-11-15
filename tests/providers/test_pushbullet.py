6317eb204c7803637340a91a3dde718e1eef3ebf
Or Carmi <or.carmi82@gmail.com>
2017-11-15 09:38:00 +0200

    skip pushbullet tests for now, figure it out later

--- tests/providers/test_pushbullet.py
+++ tests/providers/test_pushbullet.py
@@ -24,6 +24,7 @@ class TestPushbullet:
             p.notify(**data)
         assert f'\'{message}\' is a required property' in e.value.message
 
+    @pytest.mark.skip('Account is inactive for over a month, figure out how to get around this')
     @pytest.mark.online
     def test_sanity(self):
         p = get_notifier('pushbullet')
@@ -31,6 +32,7 @@ class TestPushbullet:
         rsp = p.notify(**data)
         rsp.raise_on_errors()
 
+    @pytest.mark.skip('Account is inactive for over a month, figure out how to get around this')
     @pytest.mark.online
     def test_all_options(self):
         p = get_notifier('pushbullet')
