6f26dd8c934e7515eb273e956f8d6be357679a66
orcarmi <ocarmi@proofpoint.com>
2017-12-31 11:52:07 +0200

    Bump version: 0.6.1 → 0.6.2

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def load_requirements(filename):
 
 setup(
     name='notifiers',
-    version='0.6.1',
+    version='0.6.2',
     packages=find_packages(exclude=['notifiers.tests']),
     url='https://github.com/liiight/notifiers',
     license='MIT',
