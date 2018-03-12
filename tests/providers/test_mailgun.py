ab30189956ce869ef20d81d2a1fc169e674970df
liiight <ocarmi@proofpoint.com>
2018-03-12 17:31:06 +0200

    added mailgun tests

--- tests/providers/test_mailgun.py
+++ tests/providers/test_mailgun.py
@@ -1,6 +1,9 @@
 import pytest
-
+import datetime
+import time
+from email import utils
 from notifiers.exceptions import BadArguments
+from notifiers.core import FAILURE_STATUS
 
 provider = 'mailgun'
 
@@ -25,3 +28,59 @@ class TestMailgun:
         data['env_prefix'] = 'test'
         with pytest.raises(BadArguments, match=f"'{message}' is a required property"):
             provider.notify(**data)
+
+    @pytest.mark.online
+    def test_mailgun_sanity(self, provider):
+        provider.notify(message='foo', raise_on_errors=True)
+
+    @pytest.mark.online
+    def test_mailgun_all_options(self, provider, tmpdir):
+        file_1 = tmpdir.mkdir("sub").join("hello.txt")
+        file_1.write("content")
+
+        file_2 = tmpdir.mkdir("sub").join("world.txt")
+        file_2.write("content")
+
+        now = datetime.datetime.now() + datetime.timedelta(minutes=3)
+        rfc_2822 = utils.formatdate(time.mktime(now.timetuple()))
+        data = {
+            'message': 'foo',
+            'html': f'<b>{now}</b>',
+            'subject': f'{now}',
+            'attachment': [
+                file_1.strpath,
+                file_2.strpath
+            ],
+            'inline': [
+                file_1.strpath,
+                file_2.strpath
+            ],
+            'tag': [
+                'foo',
+                'bar'
+            ],
+            'dkim': True,
+            'deliverytime': rfc_2822,
+            'testmode': False,
+            'tracking': True,
+            'tracking_click': True,
+            'tracking_opens': True,
+            'require_tls': False,
+            'skip_verification': True,
+            'headers': {
+                'foo': 'bar'
+            },
+            'data': {
+                'foo': '{"bar": true}'
+            }
+        }
+        provider.notify(**data, raise_on_errors=True)
+
+    def test_mailgun_error_response(self, provider):
+        data = {
+            'api_key': 'FOO',
+            'message': 'bla',
+        }
+        rsp = provider.notify(**data)
+        assert rsp.status == FAILURE_STATUS
+        assert 'Forbidden' in rsp.errors
