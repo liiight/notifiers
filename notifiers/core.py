858524e9d2be0f66feab450237da70ca512567a5
liiight <ocarmi@proofpoint.com>
2018-03-12 23:15:24 +0200

    tweaked schema error return message

--- notifiers/core.py
+++ notifiers/core.py
@@ -171,7 +171,8 @@ class SchemaResource(ABC):
         e = best_match(self.validator.iter_errors(data))
         if e:
             custom_error_key = f'error_{e.validator}'
-            msg = e.schema[custom_error_key] if e.schema.get(custom_error_key) else e.message
+            error = e.schema[custom_error_key] if e.schema.get(custom_error_key) else e.message
+            msg = f'Error: {error}, instance: {e.instance}'
             raise BadArguments(validation_error=msg, provider=self.name, data=data)
 
     def _validate_data_dependencies(self, data: dict) -> dict:
