736d8da86552584995df6d463a58205d212d2fdf
Yaakov Kuperman <ykuperman@gmail.com>
2018-08-30 18:17:45 -0400

    updating tests to use notify() instead of private function.

--- tests/providers/test_sendgrid.py
+++ tests/providers/test_sendgrid.py
@@ -12,6 +12,7 @@ import re
 import copy
 import base64
 import pytest
+from unittest.mock import MagicMock
 from notifiers.exceptions import BadArguments
 provider = 'sendgrid'
 
@@ -23,6 +24,7 @@ def get_basic_payload():
     having to worry about required arguments
     """
     return {
+        'api_key': '1234',
         'personalizations': [
             {
                 'to': [
@@ -59,6 +61,13 @@ def get_attachment_payload():
 
 class TestSendgridSchema:
     """Tests just the schema validation for SG"""
+
+    @pytest.fixture(autouse=True)
+    def patch_requests(self, monkeypatch):
+        def requests_return(**kwargs):
+            return MagicMock(), []
+        monkeypatch.setattr('notifiers.utils.requests.post', requests_return)
+
     old_environ = {}
 
     @classmethod
@@ -74,7 +83,6 @@ class TestSendgridSchema:
     @classmethod
     def teardown_class(cls):
         """put the NOTIFIERS variables back for other tests"""
-        print(cls.old_environ)
         for each in cls.old_environ:
             os.environ[each] = cls.old_environ[each]
 
@@ -86,7 +94,7 @@ class TestSendgridSchema:
         })
 
     def test_basic_payload(self, provider):
-        provider._process_data(**get_basic_payload())
+        rsp = provider.notify(**get_basic_payload())
 
     to_payloads = [
         (
@@ -152,7 +160,7 @@ class TestSendgridSchema:
         full_payload = get_basic_payload()
         full_payload.update(payload)
         with pytest.raises(BadArguments, match=error):
-            provider._validate_data(full_payload)
+            provider.notify(**full_payload)
 
     def test_personalization_multiple_to(self, provider):
         payload = get_basic_payload()
@@ -172,7 +180,7 @@ class TestSendgridSchema:
                 }
             ]
         })
-        assert provider._process_data(**payload) == payload
+        provider.notify(**payload)
 
     def test_personalization_simple_cc_and_bcc(self, provider):
         payload = get_basic_payload()
@@ -200,232 +208,232 @@ class TestSendgridSchema:
                 }
             ]
         })
-        assert provider._process_data(**payload) == payload
+        provider.notify(**payload)
 
     def test_personalization_headers(self, provider):
         payload = get_basic_payload()
         payload['personalizations'][0]['headers'] = {'X-Whatever': 'Value'}
-        assert provider._process_data(**payload) == payload
+        provider.notify(**payload)
 
     def test_personalization_subject(self, provider):
         payload = get_basic_payload()
         payload['personalizations'][0]['subject'] = 'my subject'
-        assert provider._process_data(**payload) == payload
+        provider.notify(**payload)
 
     def test_personalization_substitutions(self, provider):
         payload = get_basic_payload()
         payload['personalizations'][0]['substitutions'] = {'key': 'value'}
-        assert provider._process_data(**payload) == payload
+        provider.notify(**payload)
 
     def test_personalization_custom_args(self, provider):
         payload = get_basic_payload()
         payload['personalizations'][0]['custom_args'] = {'key': 'value'}
-        assert provider._process_data(**payload) == payload
+        provider.notify(**payload)
 
     def test_addtional_properties_in_personalizations(self, provider):
         payload = get_basic_payload()
         payload['personalizations'][0]['bad_data'] = 1234
         with pytest.raises(BadArguments, match="'bad_data' was unexpected"):
-            provider._process_data(**payload)
+            provider.notify(**payload)
 
     def test_multiple_personalizations(self, provider):
         payload = get_basic_payload()
         payload['personalizations'].append(payload['personalizations'][0])
-        assert provider._process_data(**payload) == payload
+        provider.notify(**payload)
 
     def test_from_missing(self, provider):
         payload = get_basic_payload()
         del payload['from']
         with pytest.raises(BadArguments,
                            match="'from' is a required property"):
-            provider._process_data(**payload)
+            provider.notify(**payload)
 
     def test_from_email_missing(self, provider):
         payload = get_basic_payload()
         del payload['from']['email']
         with pytest.raises(BadArguments,
                            match="'email' is a required property"):
-            provider._process_data(**payload)
+            provider.notify(**payload)
 
     def test_from_name(self, provider):
         payload = get_basic_payload()
         payload['from']['name'] = 'bill'
-        assert provider._process_data(**payload) == payload
+        provider.notify(**payload)
 
     def test_from_additional_properties(self, provider):
         payload = get_basic_payload()
         payload['from']['bad_data'] = 1234
         with pytest.raises(BadArguments, match="'bad_data' was unexpected"):
-            provider._process_data(**payload)
+            provider.notify(**payload)
 
     def test_reply_to_email_missing(self, provider):
         payload = get_basic_payload()
         payload['reply_to'] = {}
         with pytest.raises(BadArguments,
                            match="'email' is a required property"):
-            provider._process_data(**payload)
+            provider.notify(**payload)
 
     def test_email_name(self, provider):
         payload = get_basic_payload()
         payload['reply_to'] = {'name': 'bill', 'email': 'test@example.com'}
-        assert provider._process_data(**payload) == payload
+        provider.notify(**payload)
 
     def test_reply_to_additional_properties(self, provider):
         payload = get_basic_payload()
         payload['reply_to'] = {'bad_data': 1234}
         with pytest.raises(BadArguments, match="'bad_data' was unexpected"):
-            provider._process_data(**payload)
+            provider.notify(**payload)
 
     def test_subject_missing(self, provider):
         payload = get_basic_payload()
         del payload['subject']
         with pytest.raises(BadArguments,
                            match="'subject' is a required property"):
-            provider._process_data(**payload)
+            provider.notify(**payload)
 
     def test_empty_subject(self, provider):
         payload = get_basic_payload()
         payload['subject'] = ''
         with pytest.raises(BadArguments, match="'' is too short"):
-            provider._process_data(**payload)
+            provider.notify(**payload)
 
     def test_no_content(self, provider):
         payload = get_basic_payload()
         del payload['content']
         with pytest.raises(BadArguments,
                            match="'content' is a required property"):
-            provider._process_data(**payload)
+            provider.notify(**payload)
 
     def test_missing_mime_type(self, provider):
         payload = get_basic_payload()
         del payload['content'][0]['type']
         with pytest.raises(BadArguments,
                            match="'type' is a required property"):
-            provider._process_data(**payload)
+            provider.notify(**payload)
 
     def test_missing_content_value(self, provider):
         payload = get_basic_payload()
         del payload['content'][0]['value']
         with pytest.raises(BadArguments,
                            match="'value' is a required property"):
-            provider._process_data(**payload)
+            provider.notify(**payload)
 
     def test_empty_mime_type(self, provider):
         payload = get_basic_payload()
         payload['content'][0]['type'] = ''
         with pytest.raises(BadArguments, match="'' is too short"):
-            provider._process_data(**payload)
+            provider.notify(**payload)
 
     def test_empty_content_value(self, provider):
         payload = get_basic_payload()
         payload['content'][0]['value'] = ''
         with pytest.raises(BadArguments, match="'' is too short"):
-            provider._process_data(**payload)
+            provider.notify(**payload)
 
     def test_basic_attachment_payload(self, provider):
         payload = get_attachment_payload()
-        assert provider._process_data(**payload) == payload
+        provider.notify(**payload)
 
     def test_attachment_content_missing(self, provider):
         payload = get_attachment_payload()
         del payload['attachments'][0]['content']
         with pytest.raises(BadArguments,
                            match="'content' is a required property"):
-            provider._process_data(**payload)
+            provider.notify(**payload)
 
     def test_attachment_filename_missing(self, provider):
         payload = get_attachment_payload()
         del payload['attachments'][0]['filename']
         with pytest.raises(BadArguments,
                            match="'filename' is a required property"):
-            provider._process_data(**payload)
+            provider.notify(**payload)
 
     def test_empty_attachment_content(self, provider):
         payload = get_attachment_payload()
         payload['attachments'][0]['content'] = ''
         with pytest.raises(BadArguments, match="'' is too short"):
-            provider._process_data(**payload)
+            provider.notify(**payload)
 
     def test_empty_attachment_filename(self, provider):
         payload = get_attachment_payload()
         payload['attachments'][0]['filename'] = ''
         with pytest.raises(BadArguments, match="'' is too short"):
-            provider._process_data(**payload)
+            provider.notify(**payload)
 
     def test_attachment_extra_properties(self, provider):
         payload = get_attachment_payload()
         payload['attachments'][0]['bad_data'] = 1234
         with pytest.raises(BadArguments, match="'bad_data' was unexpected"):
-            provider._process_data(**payload)
+            provider.notify(**payload)
 
     def test_attachment_disposition_inline(self, provider):
         payload = get_attachment_payload()
         payload['attachments'][0]['disposition'] = 'inline'
-        assert provider._process_data(**payload) == payload
+        provider.notify(**payload)
 
     def test_attachment_disposition_attachment(self, provider):
         payload = get_attachment_payload()
         payload['attachments'][0]['disposition'] = 'attachment'
-        assert provider._process_data(**payload) == payload
+        provider.notify(**payload)
 
     def test_attachment_bad_disposition(self, provider):
         payload = get_attachment_payload()
         payload['attachments'][0]['disposition'] = 'bad_data'
         with pytest.raises(BadArguments, match="'bad_data' is not one of"):
-            provider._process_data(**payload)
+            provider.notify(**payload)
 
     def test_attachment_content_id(self, provider):
         payload = get_attachment_payload()
         payload['attachments'][0]['content_id'] = 'string'
-        assert provider._process_data(**payload) == payload
+        provider.notify(**payload)
 
     def test_template_id(self, provider):
         payload = get_basic_payload()
         payload['template_id'] = 'string'
-        assert provider._process_data(**payload) == payload
+        provider.notify(**payload)
 
     def test_sections(self, provider):
         payload = get_basic_payload()
         payload['sections'] = {'key': 'value', 'key2': 'value'}
-        assert provider._process_data(**payload) == payload
+        provider.notify(**payload)
 
     def test_headers(self, provider):
         payload = get_basic_payload()
         payload['headers'] = {'key': 'value', 'key2': 'value'}
-        assert provider._process_data(**payload) == payload
+        provider.notify(**payload)
 
     def test_categories(self, provider):
         payload = get_basic_payload()
         payload['categories'] = ['a', 'b', 'c']
-        assert provider._process_data(**payload) == payload
+        provider.notify(**payload)
 
     def test_non_unique_categories(self, provider):
         payload = get_basic_payload()
         payload['categories'] = ['a', 'a', 'a']
         with pytest.raises(BadArguments, match="has non-unique elements"):
-            provider._process_data(**payload)
+            provider.notify(**payload)
 
     def test_custom_args(self, provider):
         payload = get_basic_payload()
         payload['custom_args'] = {'key': 'value'}
-        assert provider._process_data(**payload) == payload
+        provider.notify(**payload)
 
     def test_send_at_invalid_timestamp(self, provider):
         payload = get_basic_payload()
         payload['send_at'] = 9999999999999999
         with pytest.raises(BadArguments,
                            match=" 9999999999999999 is not a 'timestamp'"):
-            provider._process_data(**payload)
+            provider.notify(**payload)
 
     def test_send_at_valid(self, provider):
         payload = get_basic_payload()
         payload['send_at'] = 1534297539
-        assert provider._process_data(**payload) == payload
+        provider.notify(**payload)
 
     def test_batch_id(self, provider):
         payload = get_basic_payload()
         payload['batch_id'] = 'batch id'
-        assert provider._process_data(**payload) == payload
+        provider.notify(**payload)
 
     def test_asm(self, provider):
         payload = get_basic_payload()
@@ -433,14 +441,14 @@ class TestSendgridSchema:
             'group_id': 1234,
             'groups_to_display': [1, 2, 3]
         }
-        assert provider._process_data(**payload) == payload
+        provider.notify(**payload)
 
     def test_asm_no_group_id(self, provider):
         payload = get_basic_payload()
         payload['asm'] = {}
         with pytest.raises(BadArguments,
                            match="'group_id' is a required property"):
-            provider._process_data(**payload)
+            provider.notify(**payload)
 
     def test_asm_additional_properties(self, provider):
         payload = get_basic_payload()
@@ -449,12 +457,12 @@ class TestSendgridSchema:
             'bad_data': 1
         }
         with pytest.raises(BadArguments, match="'bad_data' was unexpected"):
-            provider._process_data(**payload)
+            provider.notify(**payload)
 
     def test_ip_pool_name(self, provider):
         payload = get_basic_payload()
         payload['ip_pool_name'] = 'asdf'
-        assert provider._process_data(**payload) == payload
+        provider.notify(**payload)
 
     def test_mail_settings(self, provider):
         payload = get_basic_payload()
@@ -480,10 +488,11 @@ class TestSendgridSchema:
         }
 
         expected_payload = copy.deepcopy(payload)
+        del expected_payload['api_key']
         expected_payload['mail_settings']['sandbox_mode'] = {
             'enable': True
         }
-        assert provider._process_data(**payload) == expected_payload
+        assert provider.notify(**payload).data == expected_payload
 
     def test_tracking_settings(self, provider):
         payload = get_basic_payload()
@@ -507,29 +516,35 @@ class TestSendgridSchema:
                 'utm_campaign': 'something'
             }
         }
-        assert provider._process_data(**payload) == payload
+        provider.notify(**payload)
 
     def test_from_(self, provider):
         payload = get_basic_payload()
         payload['from_'] = payload.pop('from')
-        assert provider._process_data(**payload) == get_basic_payload()
+        expected_payload = get_basic_payload()
+        del expected_payload['api_key']
+        assert provider.notify(**payload).data == expected_payload
 
     def test_message(self, provider):
         payload = get_basic_payload()
         payload['message'] = payload['content'][0]['value']
         del payload['content']
-        assert provider._process_data(**payload) == get_basic_payload()
+        expected_payload = get_basic_payload()
+        del expected_payload['api_key']
+        assert provider.notify(**payload).data == expected_payload
 
     def test_to(self, provider):
         payload = get_basic_payload()
         payload['to'] = payload['personalizations'][0]['to'][0]['email']
         del payload['personalizations']
-        assert provider._process_data(**payload) == get_basic_payload()
+        expected_payload = get_basic_payload()
+        del expected_payload['api_key']
+        assert provider.notify(**payload).data == expected_payload
 
     def test_to_and_personalizations(self, provider):
         payload = get_basic_payload()
         payload['to'] = 'test@example.com'
-        expected_payload = payload
+        expected_payload = copy.deepcopy(payload)
         del expected_payload['to']
         expected_payload['personalizations'].append(
             {
@@ -540,12 +555,15 @@ class TestSendgridSchema:
                 ]
             }
         )
-        assert provider._process_data(**payload) == expected_payload
+        del expected_payload['api_key']
+        assert provider.notify(**payload).data == expected_payload
 
     def test_bare_from(self, provider):
         payload = get_basic_payload()
         payload['from'] = 'test@example.com'
-        assert provider._process_data(**payload) == get_basic_payload()
+        expected_payload = get_basic_payload()
+        del expected_payload['api_key']
+        assert provider.notify(**payload).data == expected_payload
 
 
 def get_online_basic_payload():
@@ -556,6 +574,7 @@ def get_online_basic_payload():
     payload = get_basic_payload()
     # delete this so that the 'to' from the environment gets read in instead
     del payload['personalizations']
+    del payload['api_key']
     return payload
 
 
@@ -570,10 +589,18 @@ class TestSendgridOnline:
     def test_full_payload(self, provider):
         # this tests all the payload options that can be used realistically
         # on the free tier
-        payload = get_basic_payload()
+        payload = get_online_basic_payload()
         # make sure the personalized email gets inboxed
-        payload['personalizations'][0]['to'][0]['email'] =\
-            os.environ.get('NOTIFIERS_SENDGRID_TO')
+        payload['personalizations'] = [
+            {
+                'to': [
+                    {
+                        'email': os.environ.get('NOTIFIERS_SENDGRID_TO')
+                    }
+                ]
+            }
+        ]
+
         payload['personalizations'][0]['substitutions'] = {
             '{{name}}': 'Kenobi',
             '{{person}}': '{{person_section}}'
