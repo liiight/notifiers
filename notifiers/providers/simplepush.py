d5442d993dc3fba1406925eda1c8c6b0423fe572
liiight <or.carmi82@gmail.com>
2020-03-01 10:57:07 +0200

    converted simplepush.py

--- notifiers/providers/simplepush.py
+++ notifiers/providers/simplepush.py
@@ -1,6 +1,16 @@
+from pydantic import Field
+
 from ..models.provider import Provider
+from ..models.provider import SchemaModel
 from ..models.response import Response
 from ..utils import requests
+
+
+class SimplePushSchema(SchemaModel):
+    key: str = Field(..., description="Your user key")
+    message: str = Field(..., description="Your message", alias="msg")
+    title: str = Field(None, description="Message title")
+    event: str = Field(None, description="Event Id")
 
 
 class SimplePush(Provider):
@@ -9,26 +19,13 @@ class SimplePush(Provider):
     base_url = "https://api.simplepush.io/send"
     site_url = "https://simplepush.io/"
     name = "simplepush"
+    path_to_errors = ("message",)
 
-    _required = {"required": ["key", "message"]}
-    _schema = {
-        "type": "object",
-        "properties": {
-            "key": {"type": "string", "title": "your user key"},
-            "message": {"type": "string", "title": "your message"},
-            "title": {"type": "string", "title": "message title"},
-            "event": {"type": "string", "title": "Event ID"},
-        },
-        "additionalProperties": False,
-    }
+    schema_model = SimplePushSchema
 
-    def _prepare_data(self, data: dict) -> dict:
-        data["msg"] = data.pop("message")
-        return data
-
-    def _send_notification(self, data: dict) -> Response:
-        path_to_errors = ("message",)
+    def _send_notification(self, data: SimplePushSchema) -> Response:
+        data = data.to_dict()
         response, errors = requests.post(
-            self.base_url, data=data, path_to_errors=path_to_errors
+            self.base_url, data=data, path_to_errors=self.path_to_errors
         )
         return self.create_response(data, response, errors)
