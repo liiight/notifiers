073523b4fcd79f1e421e9aa78fe053c10b21a98e
liiight <or.carmi82@gmail.com>
2022-02-08 23:29:29 +0200

    removed getpass.getuser() as it can cause crashes on certain conditions. Closes #328

--- notifiers/providers/email.py
+++ notifiers/providers/email.py
@@ -1,4 +1,3 @@
-import getpass
 import mimetypes
 import smtplib
 import socket
@@ -17,7 +16,7 @@ from ..utils.schema.helpers import list_to_commas
 from ..utils.schema.helpers import one_or_more
 
 DEFAULT_SUBJECT = "New email from 'notifiers'!"
-DEFAULT_FROM = f"{getpass.getuser()}@{socket.getfqdn()}"
+DEFAULT_FROM = f"notifiers@{socket.getfqdn()}"
 DEFAULT_SMTP_HOST = "localhost"
 
 
