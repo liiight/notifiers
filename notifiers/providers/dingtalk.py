054cf9c54487244f10460773270d81f0f78a2dab
Henry Guo <henry.f22@gmail.com>
2022-03-10 11:18:38 +0800

    style: improve dingtalk `msg_data` schema

--- notifiers/providers/dingtalk.py
+++ notifiers/providers/dingtalk.py
@@ -22,13 +22,85 @@ class DingTalk(Provider):
             "msg_data": {
                 "type": "object",
                 "title": "dingtalk message body definition",
+                "properties": {
+                    "msgtype": {
+                        "type": "string",
+                        "title": "choose a message type, these type supported: text, markdown, link, actionCard"
+                    },
+                    "text": {
+                        "type": "object",
+                        "title": "text message",
+                        "properties": {
+                            "content": {
+                                "type": "string",
+                                "title": "text message content"
+                            }
+                        },
+                        "required": ["content"]
+                    },
+                    "markdown": {
+                        "type": "object",
+                        "title": "markdown message",
+                        "properties": {
+                            "title": {
+                                "type": "string",
+                                "title": "message title"
+                            },
+                            "text": {
+                                "type": "string",
+                                "title": "markdown message content"
+                            }
+                        },
+                        "required": ["title", "text"]
+                    },
+                    "link": {
+                        "type": "object",
+                        "title": "link message",
+                        "properties": {
+                            "title": {
+                                "type": "string",
+                                "title": "message title"
+                            },
+                            "text": {
+                                "type": "string",
+                                "title": "message content"
+                            },
+                            "messageUrl": {
+                                "type": "string",
+                                "title": "link url"
+                            }
+                        },
+                        "required": ["title", "text", "messageUrl"]
+                    },
+                    "actionCard": {
+                        "type": "object",
+                        "title": "card message",
+                        "properties": {
+                            "title": {
+                                "type": "string",
+                                "title": "message title"
+                            },
+                            "text": {
+                                "type": "string",
+                                "title": "message content"
+                            },
+                            "singleTitle": {
+                                "type": "string",
+                                "title": "title for card footage button, like 'Read more.' button"
+                            },
+                            "singleURL": {
+                                "type": "string",
+                                "title": "link url when user click card button"
+                            }
+                        },
+                        "required": ["title", "text", "singleTitle", "singleURL"]
+                    },
+                },
+                "required": ["msgtype"]
             },
         },
         "additionalProperties": False,
     }
-
-    def _prepare_data(self, data: dict) -> dict:
-        return data
 
     def _send_notification(self, data: dict) -> Response:
         access_token = data.pop("access_token")
