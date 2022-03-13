73871a232890b3a50ce69a3f43b68a350cdad1a8
Henry Guo <henry.f22@gmail.com>
2022-03-13 23:03:34 +0800

    test: fix the test case and doc

--- tests/providers/test_dingtalk.py
+++ tests/providers/test_dingtalk.py
@@ -6,13 +6,12 @@ provider = "dingtalk"
 class TestDingTalk:
     def test_dingtalk_metadata(self, provider):
         assert provider.metadata == {
-            "base_url": "https://oapi.dingtalk.com/robot/send?access_token={}",
+            "base_url": "https://oapi.dingtalk.com/robot/send",
             "name": "dingtalk",
             "site_url": "https://oapi.dingtalk.com/",
         }
 
     @pytest.mark.online
     def test_sanity(self, provider, test_message):
-        msg_data = {"msgtype": "text", "text": {"content": test_message}}
-        data = {"access_token": "token", "msg_data": msg_data}
+        data = {"access_token": "token",  "message": test_message}
         provider.notify(**data, raise_on_errors=True)
