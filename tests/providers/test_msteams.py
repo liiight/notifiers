92ed185c4aab9ab02b0e131f359af1138a80c4dd
Jakub Cee <51480838+JakubCee@users.noreply.github.com>
2022-12-19 16:25:36 +0100

    Add simple test

--- tests/providers/test_msteams.py
+++ tests/providers/test_msteams.py
@@ -1,2 +1,21 @@
 import pytest
 
+from notifiers.exceptions import NotificationError, BadArguments
+
+provider = "msteams"
+
+
+class TestMSTeams:
+    """MS Teams tests"""
+
+    @pytest.mark.parametrize(
+        "data, missing", [
+            ({"title": "foo", "webhook_url": "bar"}, "message"),
+            ({"message": "foo"}, "webhook_url")
+        ]
+    )
+    def test_msteams_missing_required(self, data, missing, provider):
+        data["end_prefix"] = "test"
+        with pytest.raises(BadArguments) as e:
+            provider.notify(**data)
+        assert f"'{missing}' is a required property" in e.value.message
