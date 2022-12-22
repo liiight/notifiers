aa610cc335296f4214f0a1dc1c67dbbecf21bf20
Jakub Cee <51480838+JakubCee@users.noreply.github.com>
2022-12-22 14:58:28 +0100

    Teams with button support

--- notifiers/providers/msteams.py
+++ notifiers/providers/msteams.py
@@ -19,6 +19,13 @@ class MSTeams(Provider):
             },
             "message": {"type": "string", "title": "body of the notification"},
             "title": {"type": "string", "title": "title of notification"},
+            "button": {
+                "type": "object",
+                "properties": {
+                    "name": {"type": "string", "title": "Name of the button"},
+                    "target": {"type": "string", "title": "URL address of the target"}
+                }
+            }
         },
         "additionalProperties": True,
     }
@@ -26,11 +33,26 @@ class MSTeams(Provider):
     _required = {"required": ["message", "webhook_url"]}
 
     def _prepare_data(self, data: dict) -> dict:
+        # process text/message
         text = data.pop("message")
         data["text"] = text
+
+        # process button
+        if "button" in data.keys():
+            if "potentialAction" not in data.keys():
+                data["potentialAction"] = []
+            button = {
+                "@context": "http://schema.org",
+                "@type": "ViewAction",
+                "name": data["button"]["name"],
+                "target": [data["button"]["target"]]
+            }
+            data["potentialAction"].append(button)
+            data.pop("button")
         return data
 
     def _send_notification(self, data: dict) -> Response:
         url = data.pop("webhook_url")
         response, errors = requests.post(url, json=data)
         return self.create_response(data, response, errors)
+
