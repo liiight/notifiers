cb9164d52ec79d7996e948b03eb8c0f031e2b192
K0IN <thisk0in@gmail.com>
2023-12-26 01:57:44 +0100

    Update test_notify.py

--- tests/providers/test_notify.py
+++ tests/providers/test_notify.py
@@ -4,7 +4,5 @@ provider = "notify"
 
 
 class TestNotify:
-    @pytest.mark.online
-    def test_sanity(self, provider, test_message):
-        data = {"message": test_message}
-        provider.notify(**data, raise_on_errors=True)
+    pass
+
